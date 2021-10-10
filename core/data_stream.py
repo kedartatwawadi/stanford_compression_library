a36d35cc5484dfb0f9a3fcf720e579e44e2b22d0
Kedar Tatwawadi <kedar@kedars-mbp.lan>
2021-10-10 15:30:08 -0700

    split into files

--- core/data_stream.py
+++ core/data_stream.py
@@ -1,44 +1,15 @@
-import numpy as np
 from typing import List, Set
 from dataclasses import dataclass
 from core.util import compute_alphabet, compute_counts_dict
+from core.prob_dist import ProbabilityDist
 
-class ProbabilityDist:
-    """
-    TODO: add description
-    """
-    def __init__(self, prob_dict):
-        self.prob_dict = prob_dict
-        self._validate_prob_dist(self.prob_dict)
-    
-    @property
-    def entropy(self):
-        entropy = 0
-        for _, prob in self.prob_dict.items():
-            entropy += -prob* np.log2(prob)
-        return entropy
-
-    @staticmethod
-    def _validate_prob_dist(prob_dict):
-        """
-        checks if each value of the prob dist is non-negative, 
-        and the dist sums to 1
-        """
-
-        sum_of_probs = 0
-        for _, prob in prob_dict.items():
-            assert prob >= 0, "probabilities cannot be negative"
-            sum_of_probs += prob
-        
-        #FIXME: check if this needs a tolerance range
-        assert sum_of_probs == 1.0, "probabilities should sum to 1.0"
-  
 
 @dataclass
 class DataStream:
     """
     FIXME: for now data is only presented as a list of symbols
     """
+
     data_list: List = None
     alphabet: Set = None
 
@@ -47,7 +18,7 @@ class DataStream:
 
     @property
     def size(self):
-        return len(self.data_list)    
+        return len(self.data_list)
 
     def get_counts(self, order=0):
         """
@@ -64,15 +35,15 @@ class DataStream:
         """
         if order != 0:
             raise NotImplementedError("[order != 0] Entropy computation not implemented")
-        
+
         # get counts
         counts_dict = self.get_counts()
-        
+
         # compute the prob form the counts
         prob_dict = {}
         for symbol, count in counts_dict.items():
-            prob_dict[symbol] = count/self.size
-        
+            prob_dict[symbol] = count / self.size
+
         return ProbabilityDist(prob_dict)
 
     def get_entropy(self, order=0):
@@ -81,8 +52,6 @@ class DataStream:
         """
         if order != 0:
             raise NotImplementedError("[order != 0] Entropy computation not implemented")
-        
+
         prob_dist = self.get_empirical_distribution()
         return prob_dist.entropy
-
-
--- core/data_stream_tests.py
+++ core/data_stream_tests.py
@@ -1,29 +1,5 @@
 import unittest
-from core.data_stream import ProbabilityDist, DataStream
-
-
-class ProbabilityDistTest(unittest.TestCase):
-
-    def test_creation_entropy(self):
-        """
-        checks if the creation and validity checks are passing for valid distribution
-        """
-
-        # create valid distributions for testing
-        fair_coin_dist = ProbabilityDist({"H": 0.5, "T": 0.5})
-        assert fair_coin_dist.entropy == 1.0
-
-        dyadic_dist = ProbabilityDist({"A": 0.5, "B": 0.25, "C": 0.25})
-        print(dyadic_dist.entropy)
-    
-
-    @unittest.expectedFailure
-    def test_validation_failure(self):
-        """
-        test if init fails for incorrect distributions
-        """
-
-        dist_1 = ProbabilityDist({"H": 0.5, "T": 0.4})
+from core.data_stream import DataStream
 
 
 class DataStreamTest(unittest.TestCase):
@@ -33,8 +9,8 @@ class DataStreamTest(unittest.TestCase):
     """
 
     def test_data_stream_basic_ops(self):
-        data_list = [0,1,0,0,1,1]
-        
+        data_list = [0, 1, 0, 0, 1, 1]
+
         # create data stream object
         data_stream = DataStream(data_list)
 
@@ -44,7 +20,7 @@ class DataStreamTest(unittest.TestCase):
         # check counts
         counts_dict = data_stream.get_counts(order=0)
         assert counts_dict[0] == 3
-    
+
         # check empirical dist
         prob_dist = data_stream.get_empirical_distribution(order=0)
         assert prob_dist.prob_dict[0] == 0.5
