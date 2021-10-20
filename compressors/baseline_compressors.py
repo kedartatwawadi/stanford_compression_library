82b7fe5bf47a37d54123f0415f35de332cd4fed7
kedartatwawadi <ktatwawadi@gmail.com>
2021-10-19 22:19:37 -0700

    add comments

--- compressors/baseline_compressors.py
+++ compressors/baseline_compressors.py
@@ -1,3 +1,8 @@
+"""
+Contains some elementary baseline compressors
+1. Fixed bit width compressor 
+"""
+
 import numpy as np
 from core.data_transformer import (
     BitsToBitstringTransformer,
@@ -12,13 +17,19 @@ from core.data_compressor import DataCompressor
 
 class FixedBitwidthCompressor(DataCompressor):
     """
-    First example of a simple compressor.
     Encodes each symbol of the data to a fixed bitwidth
+    Example: data = [A,B,B,A,C]
+    Encoding ~ ['00',`01`, `01`, `00`, `10`] -> converted to bits
+    See baseline_compressors_tests.py for usage
     """
 
     def set_encoder_decoder_params(self, data_stream):
         """
-        TODO: add stuff here
+        Each compressor consists of encoder_transform + decoder_transform
+        In this case we need to know the input alphabet size to define the transforms
+
+        We first get the alphabet size and then set the encoder and decoder transforms
+        in this function
         """
         # set bit width
         alphabet = data_stream.get_alphabet()
@@ -33,6 +44,12 @@ class FixedBitwidthCompressor(DataCompressor):
             self.decoder_lookup_table[id] = a
 
         # create encoder and decoder transforms
+        # The encoder transformer is represented as a cascade:
+        # Example:
+        # data = [A,B,B,A,C]
+        # Transform-1: Lookuptable to get he alphabet index {A: 0, B: 1, C: 2}
+        # Transform-2: Uint to bitstring with a fixed width (2 for example) [0 -> 00, 1 -> 01, 2 -> 10]
+        # Transform-3: Bitstring to bits: [000 -> 0,0,0]
         self.encoder_transform = CascadeTransformer(
             [
                 LookupTableTransformer(self.encoder_lookup_table),
@@ -42,6 +59,7 @@ class FixedBitwidthCompressor(DataCompressor):
         )
 
         # create decoder transform
+        # The decoder transform is also a cascade, but in the reverse order
         self.decoder_transform = CascadeTransformer(
             [
                 BitsToBitstringTransformer(bit_width=self.bit_width),
