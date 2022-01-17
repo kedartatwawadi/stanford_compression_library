4210f30c3eea98498b1774160ba835dd0847941e
Shubham Chandak <schandak@stanford.edu>
2022-01-17 11:55:20 -0800

    add implementation of Golomb code

--- compressors/prefix_free_compressors.py
+++ compressors/prefix_free_compressors.py
@@ -7,6 +7,7 @@ from core.data_transformer import (
 )
 from core.data_stream import UintDataStream
 from core.util import bitstring_to_uint, uint_to_bitstring
+import math
 
 
 class UniversalUintCompressor(DataCompressor):
@@ -48,6 +49,87 @@ class UniversalUintCompressor(DataCompressor):
         # decode the symbol
         bitstring = "".join(data_stream.data_list[start_ind + num_ones + 1 : new_start_ind])
         symbol = bitstring_to_uint(bitstring)
+
+        return symbol, new_start_ind
+
+    def set_encoder_decoder_params(self, data_stream):
+
+        assert isinstance(data_stream, UintDataStream)
+
+        # create encoder and decoder transforms
+        self.encoder_transform = CascadeTransformer(
+            [
+                LookupFuncTransformer(self.encoder_lookup_func),
+                BitstringToBitsTransformer(),
+            ]
+        )
+
+        # create decoder transform
+        self.decoder_transform = BitsParserTransformer(self.decoder_bits_parser)
+
+
+class GolombUintCompressor(DataCompressor):
+    """
+    Golomb code with parameter M based on
+    https://en.wikipedia.org/wiki/Golomb_coding#Simple_algorithm:
+    If M is power of 2, we simplify to Rice codes with slight
+    change in logic.
+    """
+
+    def __init__(self, M: int):
+        assert M > 0
+        self.M = M
+        self.b = int(math.floor(math.log2(self.M)))
+        self.cutoff = 2 ** (self.b + 1) - self.M
+        if self.cutoff == self.M:
+            self.rice_code = True
+        else:
+            self.rice_code = False
+
+    def encoder_lookup_func(self, x: int):
+        assert x >= 0
+        assert isinstance(x, int)
+
+        q = x // self.M  # quotient
+        r = x % self.M  # remainder
+
+        # encode quotient in unary
+        quotient_bitstring = q * "1" + "0"
+        # encode remainder in binary using b bits if r < cutoff,
+        # or else encode r + cutoff using b+1 bits
+        # This is the https://en.wikipedia.org/wiki/Truncated_binary_encoding
+        # For M power of 2 (Rice code, always go with b bits)
+        if self.rice_code or r < self.cutoff:
+            remainder_bitstring = uint_to_bitstring(r, bit_width=self.b)
+        else:
+            remainder_bitstring = uint_to_bitstring(r + self.cutoff, bit_width=self.b + 1)
+
+        return quotient_bitstring + remainder_bitstring
+
+    def decoder_bits_parser(self, data_stream, start_ind):
+
+        # infer the quotient
+        quotient = 0
+        for ind in range(start_ind, data_stream.size):
+            bit = data_stream.data_list[ind]
+            if str(bit) == "0":
+                break
+            quotient += 1
+
+        current_ind = start_ind + quotient + 1
+
+        # see if next bit is 0 or 1 to figure out if we encoded remainder with b or b+1 bits
+        # For M power of 2 (Rice code, always go with b bits)
+        if self.rice_code or str(data_stream.data_list[current_ind]) == "0":
+            new_start_ind = current_ind + self.b
+            remainder_bitstring = "".join(data_stream.data_list[current_ind:new_start_ind])
+            remainder = bitstring_to_uint(remainder_bitstring)
+        else:
+            new_start_ind = current_ind + self.b + 1
+            remainder_bitstring = "".join(data_stream.data_list[current_ind:new_start_ind])
+            remainder = bitstring_to_uint(remainder_bitstring) - self.cutoff
+
+        symbol = self.M * quotient + remainder
 
         return symbol, new_start_ind
 
--- compressors/prefix_free_compressors_tests.py
+++ compressors/prefix_free_compressors_tests.py
@@ -1,5 +1,8 @@
 import unittest
-from compressors.prefix_free_compressors import UniversalUintCompressor
+from compressors.prefix_free_compressors import (
+    GolombUintCompressor,
+    UniversalUintCompressor,
+)
 from core.data_stream import UintDataStream
 
 
@@ -24,3 +27,40 @@ class UintUniversalCompressorTest(unittest.TestCase):
         # check if the encoding/decoding was lossless
         for inp_symbol, out_symbol in zip(data_stream.data_list, decoded_stream.data_list):
             assert inp_symbol == out_symbol
+
+
+class GolombUintCompressorTest(unittest.TestCase):
+    """
+    FIXME: @tpulkit -> improve these tests
+    """
+
+    def _test_encode_decode_helper(M, data_list, expected_output_bitstring):
+        compressor = GolombUintCompressor(M)
+
+        # sample data
+        data_stream = UintDataStream(data_list)
+
+        # test encode
+        output_bits_stream = compressor.encode(data_stream)
+
+        assert "".join(output_bits_stream.data_list) == expected_output_bitstring
+
+        # test decode
+        decoded_stream = compressor.decode(output_bits_stream)
+
+        # check if the encoding/decoding was lossless
+        for inp_symbol, out_symbol in zip(data_stream.data_list, decoded_stream.data_list):
+            assert inp_symbol == out_symbol
+
+    def test_encode_decode(self):
+        # first test with M power of 2
+        M = 4  # so b = 2 and cutoff = 4 (cutoff can be ignored for M power of 2 which is just Rice code)
+        data_list = [0, 1, 4, 102]
+        expected_output_bitstring = "000" + "001" + "1000" + "1" * 25 + "0" + "10"
+        GolombUintCompressorTest._test_encode_decode_helper(M, data_list, expected_output_bitstring)
+
+        # test with M not power of 2
+        M = 10  # so b = 3 and cutoff = 6
+        data_list = [2, 7, 26, 102]
+        expected_output_bitstring = "0010" + "01101" + "1101100" + "11111111110010"
+        GolombUintCompressorTest._test_encode_decode_helper(M, data_list, expected_output_bitstring)
