153b30d6dbab6259445786b2843536e3ac346683
kedartatwawadi <ktatwawadi@gmail.com>
2022-02-12 13:39:44 -0800

    add comments

--- core/data_block.py
+++ core/data_block.py
@@ -1,11 +1,20 @@
-from typing import List, Set
+from typing import List, Mapping, Set
 from core.prob_dist import ProbabilityDist
 import unittest
 
 
 class DataBlock:
     """
-    wrapper around a list of symbols
+    wrapper around a list of symbols.
+
+    The class is a wrapper around a list of symbols (self.data_list). The data_block is typically used
+    to represent input to the data encoders (or output from data decoders)
+
+    Some utility functions (useful generally for compression) implemented are:
+    - size
+    - alphabet
+    - empirical_distribution
+    - entropy
     """
 
     def __init__(self, data_list: List):
@@ -16,15 +25,30 @@ class DataBlock:
         return len(self.data_list)
 
     def get_alphabet(self) -> Set:
+        """returns the set of unique symbols in the data_list
+
+        Returns:
+            Set: the alphabet
+        """
         alphabet = set()
         for d in self.data_list:
             alphabet.add(d)
         return alphabet
 
     def get_counts(self, order=0):
+        """returns a dictionary of counts for symbols in self.data_list
+
+        Args:
+            order (int, optional): (the order of k-mer for which counts need to be obtained) Defaults to 0.
+            order 0-> symbol counts.. order 1-> tuple counts
+
+        Raises:
+            NotImplementedError: If order !=0, as it is not implemented yet
+
+        Returns:
+            dict: {symbol:count, ...}
         """
-        returns counts for each object
-        """
+
         if order != 0:
             raise NotImplementedError("[order != 0] counts not implemented")
 
@@ -41,11 +65,24 @@ class DataBlock:
         return count_dict
 
     def get_empirical_distribution(self, order=0) -> ProbabilityDist:
+        """get empirical distribution for self.data_list of order 0
+
+        Return the empirical distribution of the given order.
+        for e.g if data_list = [A,B,A,A], order=0 -> returns {A:0.75, B:0.25}.
+        if data_list = [A,B,A,A], order = 1 -> returns {AB:0.33, BA:0.33, AA:0.33}
+
+        Args:
+            order (int, optional): the order of counts. Defaults to 0.
+
+        Raises:
+            NotImplementedError: not implemented for order != 0 yet
+
+        Returns:
+            ProbabilityDist: the epirical distribution
         """
-        Computes the empirical distribution of the given order
-        """
+
         if order != 0:
-            raise NotImplementedError("[order != 0] Entropy computation not implemented")
+            raise NotImplementedError("[order != 0] empirical counts not implemented")
 
         # get counts
         counts_dict = self.get_counts()
@@ -58,8 +95,10 @@ class DataBlock:
         return ProbabilityDist(prob_dict)
 
     def get_entropy(self, order=0):
-        """
-        Computes the entropy of the given order
+        """Returns the entropy of the empirical_distribution of data_list
+
+        returns the entropy of the empirical_distribution
+        https://en.wikipedia.org/wiki/Entropy_(information_theory)
         """
         if order != 0:
             raise NotImplementedError("[order != 0] Entropy computation not implemented")
@@ -68,29 +107,24 @@ class DataBlock:
         return prob_dist.entropy
 
 
-class DataBlockTest(unittest.TestCase):
-    """
-    checks basic operations for a DataBlock
-    FIXME: improve these tests
-    """
+def test_data_block_basic_ops():
+    """checks basic operations for a DataBlock"""
+    data_list = [0, 1, 0, 0, 1, 1]
 
-    def test_data_block_basic_ops(self):
-        data_list = [0, 1, 0, 0, 1, 1]
+    # create data block object
+    data_block = DataBlock(data_list)
 
-        # create data block object
-        data_block = DataBlock(data_list)
+    # check size
+    assert data_block.size == 6
 
-        # check size
-        assert data_block.size == 6
+    # check counts
+    counts_dict = data_block.get_counts(order=0)
+    assert counts_dict[0] == 3
 
-        # check counts
-        counts_dict = data_block.get_counts(order=0)
-        assert counts_dict[0] == 3
+    # check empirical dist
+    prob_dist = data_block.get_empirical_distribution(order=0)
+    assert prob_dist.prob_dict[0] == 0.5
 
-        # check empirical dist
-        prob_dist = data_block.get_empirical_distribution(order=0)
-        assert prob_dist.prob_dict[0] == 0.5
-
-        # check entropy
-        entropy = data_block.get_entropy(order=0)
-        assert entropy == 1.0
+    # check entropy
+    entropy = data_block.get_entropy(order=0)
+    assert entropy == 1.0
--- core/data_stream.py
+++ core/data_stream.py
@@ -1,25 +1,47 @@
 import abc
 import tempfile
 import os
-from typing import List
+import typing
 from core.data_block import DataBlock
+
+Symbol = typing.Any
 
 
 class DataStream(abc.ABC):
-    """
-    Class to represent the input stream
+    """abstract class to represent a Data Stream
+
+    The DataStream facilitates the block interface.
+    From the interface standpoint, the two functions which are useful are:
+    - get_block(block_size) -> returns a DataBlock of the given block_size from the stream
+    - write_block(block) -> writes the block of data to the stream
+
+    The DataStream can act as a stream object for both writing and reading blocks
+    The two more useful sub-classes of the abstract class are FileDataStream and ListDataStream.
+    (see their description for more details)
     """
 
     @abc.abstractmethod
     def reset(self):
-        # resets the data stream
+        """resets the data stream"""
         pass
 
     @abc.abstractmethod
     def get_symbol(self):
-        pass  # returns None if the stream is finished
+        """returns a symbol from the data stream, returns None if the stream is finished
 
-    def get_block(self, block_size: int):
+        This is an abstract method, and hence needs to be implemented by the subclasses
+        """
+        pass
+
+    def get_block(self, block_size: int) -> DataBlock:
+        """returns a block of data (of the given max size) from the stream
+
+        Args:
+            block_size (int): the size of the block of data to be returned.
+
+        Returns:
+            DataBlock:
+        """
         # returns the next data block
         data_list = []
         for _ in range(block_size):
@@ -37,25 +59,59 @@ class DataStream(abc.ABC):
 
     @abc.abstractmethod
     def write_symbol(self, s):
+        """writes the given symbol to the stream
+
+        The symbol can be appropriately converted to a particular format before writing.
+        This is an abstract method and so, the subclass will have to implement it
+
+        Args:
+            s (Any): symbol to be written to the stream
+        """
         pass
 
     def write_block(self, data_block: DataBlock):
+        """write the input block to the stream
+
+        Args:
+            data_block (DataBlock): block to be written to the stream
+        """
         for s in data_block.data_list:
             self.write_symbol(s)
 
     def __enter__(self):
+        """function executed while opening the context"""
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
+        """Function executed which exiting the context
+
+        Note that the arguments exc_type, exc_value, exc_traceback are as required by python for a context
+        """
         pass
 
 
 class ListDataStream(DataStream):
     """
-    create a data stream object from a list
+    ListDataStream is a wrapper around a list of symbols.
+    It is useful to:
+    - extract data from the list block by block
+    - write data to the list block by block
+
+    In practice, this class might be used mainly for testing
+    (as usually you would read data from a file.. see FileDataStream for that)
     """
 
-    def __init__(self, input_list: List):
+    def __init__(self, input_list: typing.List):
+        """initialize with input_list and reset the stream
+
+        Args:
+            input_list (List): the list of symbols, around which the class is a wrapper
+
+        Usage:
+            with ListDataStream(input_list) as ds:
+                block = ds.get_block(block_size=5)
+                # do something with the block
+        """
         # assert whether the input_list is indeed a list
         assert isinstance(input_list, list)
         self.input_list = input_list
@@ -64,70 +120,104 @@ class ListDataStream(DataStream):
         self.reset()
 
     def reset(self):
+        """resets the current_ind counter"""
         self.current_ind = 0
 
-    def get_symbol(self):
+    def get_symbol(self) -> Symbol:
+        """returns the next symbol from the self.input_list"""
+
+        # retrieve the next symbol
         if self.current_ind >= len(self.input_list):
             return None
         s = self.input_list[self.current_ind]
+
+        # increment the current_ind counter
         self.current_ind += 1
         return s
 
-    def write_symbol(self, s):
+    def write_symbol(self, s: Symbol):
+        """adds a symbol to the stream"""
         self.input_list.append(s)
 
 
 class FileDataStream(DataStream):
-    """
-    create a data stream object from a file
+    """Abstract class to create a data stream from a File
+
+    The FileDataStream defines __exit__, __enter__ methods on top of DataStream.
+    These methods handle file obj opening/closing
+
+    Subclasses (eg: TextDataStream) need to imeplement methods get_symbol, write_symbol
+    to get a functional object.
     """
 
     def __init__(self, file_path: str, permissions="r"):
-        """
-        block class -> specifies what type of block to return
-        Also, every DataBlock has a char_to_symbol function which is used to convert data appropriately before passing
+        """Initialize the FileDataStream object
+
+        Args:
+            file_path (str): path of the file to read from/write to
+            permissions (str, optional): Permissions to open the file obj. Use "r" to read, "w" to write to
+            (other pyhton file obj permissions also can be used). Defaults to "r".
         """
         self.file_path = file_path
         self.permissions = permissions
 
     def __enter__(self):
+        """open the file object context based on the permissions specified
+
+        Example:
+        with TextFileDataStream(path, "w") as fds:
+            # get a text block
+            block = fds.get_block(5)
+
+        """
         self.file_obj = open(self.file_path, self.permissions)
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
+        """close the file object at the end of context"""
         self.file_obj.close()
 
     def reset(self):
+        """resets the file object to the beginning"""
         self.file_obj.seek(0)
 
 
 class TextFileDataStream(FileDataStream):
-    """
-    reads one symbol at a time
-    """
+    """FileDataStream to read/write text data"""
 
     def get_symbol(self):
+        """get the next character from the text file
+
+        as we read character data from file by default, the get_symbol function does not need to do anything special
+        conversions
+
+        Returns:
+            (str, None): the next character, None if we reached the end of stream
+        """
         s = self.file_obj.read(1)
         if not s:
             return None
         return s
 
     def write_symbol(self, s):
+        """write a character to the text file"""
         self.file_obj.write(s)
 
 
 class Uint8FileDataStream(FileDataStream):
-    """
-    reads Uint8 numbers written to a file
+    """reads Uint8 numbers written to a file
+
+    FIXME: need to immplement
     """
 
     pass
 
 
+#################################
+
+
 def test_list_data_stream():
-    """
-    simple testing function to check if list data stream is getting generated correctly
-    """
+    """simple testing function to check if list data stream is getting generated correctly"""
     input_list = list(range(10))
     with ListDataStream(input_list) as ds:
         for i in range(3):
@@ -142,9 +232,7 @@ def test_list_data_stream():
 
 
 def test_file_data_stream():
-    """
-    function to test file data stream
-    """
+    """function to test file data stream"""
 
     # create a temporary file
     with tempfile.TemporaryDirectory() as tmpdirname:
