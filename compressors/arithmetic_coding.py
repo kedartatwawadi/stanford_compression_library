3ab19e189a8645173e0d1c468ced0687bec50d7e
kedartatwawadi <ktatwawadi@gmail.com>
2022-05-23 07:52:15 -0700

    add adaptive variant

--- compressors/arithmetic_coding.py
+++ compressors/arithmetic_coding.py
@@ -5,11 +5,17 @@ from core.data_encoder_decoder import DataDecoder, DataEncoder
 from utils.bitarray_utils import BitArray, uint_to_bitarray, bitarray_to_uint
 from core.data_block import DataBlock
 from core.prob_dist import Frequencies, get_mean_log_prob
-from utils.test_utils import get_random_data_block, try_lossless_compression
+from utils.test_utils import lossless_entropy_coder_test
+import abc
+import copy
 
 
 @dataclass
 class AECParams:
+    """AEC hyper parameters
+
+    These are a couple of parameters used by the AEC encoder. more details inline
+    """
 
     # represents the number of bits used to represent the size of the input data_block
     DATA_BLOCK_SIZE_BITS: int = 32
@@ -21,6 +27,88 @@ class AECParams:
         self.FULL: int = 1 << self.PRECISION
         self.HALF: int = 1 << (self.PRECISION - 1)
         self.QTR: int = 1 << (self.PRECISION - 2)
+        self.MAX_ALLOWED_TOTAL_FREQ: int = self.QTR
+        self.MAX_BLOCK_SIZE: int = 1 << self.DATA_BLOCK_SIZE_BITS
+
+
+class FreqModelBase(abc.ABC):
+    """Base Freq Model
+
+    The AEC encoder can be thought of consisting of two parts:
+    1. The probability model
+    2. The "lossless coding" algorithm which uses these probabilities
+
+    Note that the probabilities/frequencies coming from the probability model are fixed in the simplest Arithmetic coding version,
+    but they can be modified as we parse each symbol.
+    This class represents a generic "probability Model", but using frequencies (and hence the name FreqModel).
+    (Frequencies are used, mainly because floating point values can be unpredictable/uncertain on different platforms.)
+
+    Some typical examples of Freq models are:
+
+    a) FixedFreqModel -> the probability model is fixed to the initially provided one and does not change
+    b) AdaptiveIIDFreqModel -> starts with some initial probability distribution provided (the initial distribution is typically uniform)
+       The Adaptive Model then updates the model based on counts of the symbols it sees
+
+
+    Args:
+        freq_initial -> the frequencies used to initialize the model
+        max_allowed_total_freq -> to limit the total_freq values of the frequency model
+    """
+
+    def __init__(self, freqs_initial: Frequencies, max_allowed_total_freq):
+        # initialize the current frequencies using the initial freq.
+        # NOTE: the deepcopy here is needed as we modify the frequency table internally
+        # so, if it is used elsewhere externally, then it can cause unexpected issued
+        self.freqs_current = copy.deepcopy(freqs_initial)
+        self.max_allowed_total_freq = max_allowed_total_freq
+
+    @abc.abstractmethod
+    def update_model(self, s):
+        """updates self.freqs
+
+        Takes in as input the next symbol s and updates the
+        probability distribution self.freqs (represented in terms of frequencies)
+        appropriately. (see examples below)
+        """
+        raise NotImplementedError  # update the probability model here
+
+
+class FixedFreqModel(FreqModelBase):
+    def update_model(self, s):
+        """function to update the probability model
+
+        In this case, we don't do anything as the freq model is fixed
+
+        Args:
+            s (Symbol): the next symbol
+        """
+        # nothing to do here as the freqs are always fixed
+        pass
+
+
+class AdaptiveIIDFreqModel(FreqModelBase):
+    def update_model(self, s):
+        """function to update the probability model
+
+        - We start with uniform distribution on all symbols
+        ```
+        Freq = [A:1,B:1,C:1,D:1] for example.
+        ```
+        - Every time we see a symbol, we update the freq count by 1
+        - Arithmetic coder requires the `total_freq` to remain below a certain value
+        If the total_freq goes beyond, then we divide all freq by 2 (keeping minimum freq to 1)
+
+        Args:
+            s (Symbol): the next symbol
+        """
+        # updates the model based on the next symbol
+        self.freqs_current.freq_dict[s] += 1
+
+        # if total_freq goes beyond a certain value, divide by 2
+        # NOTE: there can be different strategies here
+        if self.freqs_current.total_freq >= self.max_allowed_total_freq:
+            for s, f in self.freqs_current.freq_dict.items():
+                self.freqs_current.freq_dict[s] = max(f // 2, 1)
 
 
 class ArithmeticEncoder(DataEncoder):
@@ -33,15 +121,12 @@ class ArithmeticEncoder(DataEncoder):
     - There is of course the original paper: https://web.stanford.edu/class/ee398a/handouts/papers/WittenACM87ArithmCoding.pdf
     """
 
-    def __init__(self, params: AECParams, freqs: Frequencies):
-        super().__init__()
-        self.freqs = freqs
+    def __init__(self, params: AECParams, freq_base: Frequencies, freq_model_cls: FreqModelBase):
         self.params = params
 
-        # ensure freqs.total_freq is not too big
-        err_msg = """the frequency total is too large, which might cause stability issues. 
-        Please increase the precision (or reduce the total_freq"""
-        assert (self.freqs.total_freq << 2) < self.params.FULL, err_msg
+        # define the probability model used by the AEC
+        # the model can get updated when we call update_model(s) after every step
+        self.freq_model = freq_model_cls(freq_base, params.MAX_ALLOWED_TOTAL_FREQ)
 
     @classmethod
     def shrink_range(cls, freqs: Frequencies, s: Any, low: int, high: int) -> Tuple[int, int]:
@@ -53,6 +138,7 @@ class ArithmeticEncoder(DataEncoder):
         Returns:
             Tuple[int, int]: (low, high) ranges returned after shrinking
         """
+
         # compute some intermediate variables: rng, c, d
         rng = high - low
         c = freqs.cumulative_freq_dict[s]
@@ -69,7 +155,7 @@ class ArithmeticEncoder(DataEncoder):
 
         # ensure data_block.size is not too big
         err_msg = "choose a larget DATA_BLOCK_SIZE_BITS, as data_block.size is too big"
-        assert data_block.size < (1 << self.params.DATA_BLOCK_SIZE_BITS), err_msg
+        assert data_block.size < (1 << self.params.MAX_BLOCK_SIZE), err_msg
 
         # initialize the low and high states
         low = 0
@@ -91,9 +177,18 @@ class ArithmeticEncoder(DataEncoder):
         # start the encoding
         for s in data_block.data_list:
 
+            # ensure freqs.total_freq is not too big
+            err_msg = """the frequency total is too large, which might cause stability issues. 
+            Please increase the precision (or reduce the total_freq"""
+            assert (
+                self.freq_model.freqs_current.total_freq < self.params.MAX_ALLOWED_TOTAL_FREQ
+            ), err_msg
+
             # shrink range
             # i.e. the core Arithmetic encoding step
-            low, high = ArithmeticEncoder.shrink_range(self.freqs, s, low, high)
+            low, high = ArithmeticEncoder.shrink_range(self.freq_model.freqs_current, s, low, high)
+            ## update the freq model for encoding the next symbol
+            self.freq_model.update_model(s)
 
             # perform re-normalizing range
             # NOTE: the low, high values need to be re-normalized as else they will keep shrinking
@@ -148,12 +243,11 @@ class ArithmeticDecoder(DataDecoder):
     - Charles Bloom's blog: https://www.cbloom.com/algs/statisti.html#A5
     """
 
-    def __init__(self, params: AECParams, freqs: Frequencies):
-        super().__init__()
-        self.freqs = freqs
+    def __init__(self, params: AECParams, freq_base: Frequencies, freq_model_cls: FreqModelBase):
         self.params = params
+        self.freq_model = freq_model_cls(freq_base, params.MAX_ALLOWED_TOTAL_FREQ)
 
-    def decode_step_core(self, low: int, high: int, state: int):
+    def decode_step_core(self, low: int, high: int, state: int, freqs: Frequencies):
         """Core Arithmetic decoding function
 
         We cut the [low, high) range bits proportional to the cumulative probability of each symbol
@@ -173,12 +267,10 @@ class ArithmeticDecoder(DataDecoder):
         # FIXME: simplify this search.
         rng = high - low
         search_list = (
-            low
-            + (np.array(list(self.freqs.cumulative_freq_dict.values())) * rng)
-            // self.freqs.total_freq
+            low + (np.array(list(freqs.cumulative_freq_dict.values())) * rng) // freqs.total_freq
         )
         start_bin = np.searchsorted(search_list, state, side="right") - 1
-        s = self.freqs.alphabet[start_bin]
+        s = freqs.alphabet[start_bin]
         return s
 
     def decode_block(self, encoded_bitarray: BitArray):
@@ -212,9 +304,12 @@ class ArithmeticDecoder(DataDecoder):
         # main decoding loop
         while True:
             # decode the next symbol
-            s = self.decode_step_core(low, high, state)
-            low, high = ArithmeticEncoder.shrink_range(self.freqs, s, low, high)
+            s = self.decode_step_core(low, high, state, self.freq_model.freqs_current)
+            low, high = ArithmeticEncoder.shrink_range(self.freq_model.freqs_current, s, low, high)
             decoded_data_list.append(s)
+
+            # update the freq_model
+            self.freq_model.update_model(s)
 
             # break when we have decoded all the symbols in the data block
             if len(decoded_data_list) == input_data_block_size:
@@ -280,7 +375,7 @@ def test_arithmetic_coding():
     """
 
     # trying out some random frequencies
-    freqs_list = [
+    data_freqs_list = [
         Frequencies({"A": 1, "B": 1, "C": 2}),
         Frequencies({"A": 12, "B": 34, "C": 1, "D": 45}),
         Frequencies({"A": 34, "B": 35, "C": 546, "D": 1, "E": 13, "F": 245}),
@@ -295,32 +390,23 @@ def test_arithmetic_coding():
     ]
 
     DATA_SIZE = 1000
-    for freq, params in zip(freqs_list, params_list):
-        # generate random data
-        prob_dist = freq.get_prob_dist()
-        data_block = get_random_data_block(prob_dist, DATA_SIZE, seed=0)
-        avg_log_prob = get_mean_log_prob(prob_dist, data_block)
-
-        # create encoder decoder
-        encoder = ArithmeticEncoder(params, freq)
-        decoder = ArithmeticDecoder(params, freq)
-
-        is_lossless, encode_len, _ = try_lossless_compression(
-            data_block, encoder, decoder, add_extra_bits_to_encoder_output=True
+    for freq, params in zip(data_freqs_list, params_list):
+        # create encoder/decoder
+        encoder = ArithmeticEncoder(params, freq, FixedFreqModel)
+        decoder = ArithmeticDecoder(params, freq, FixedFreqModel)
+        lossless_entropy_coder_test(
+            encoder, decoder, freq, DATA_SIZE, encoding_optimality_precision=1e-1, seed=0
         )
 
-        # avg codelen ignoring the bits used to signal num data elements
-        avg_codelen = (encode_len - params.DATA_BLOCK_SIZE_BITS) / data_block.size
-        print(
-            f"Arithmetic coding: avg_log_prob={avg_log_prob:.3f}, AEC codelen(without header): {avg_codelen:.3f}"
+    ## create adaptive coder
+    for freq, params in zip(data_freqs_list, params_list):
+        # create encoder/decoder
+        encoder = ArithmeticEncoder(
+            params, Frequencies({a: 1 for a in freq.alphabet}), AdaptiveIIDFreqModel
         )
-
-        # check whether arithmetic coding results are close to optimal codelen
-        np.testing.assert_almost_equal(
-            avg_codelen,
-            avg_log_prob,
-            decimal=2,
-            err_msg="Arithmetic coding is not close to avg codelen",
+        decoder = ArithmeticDecoder(
+            params, Frequencies({a: 1 for a in freq.alphabet}), AdaptiveIIDFreqModel
         )
-
-        assert is_lossless
+        lossless_entropy_coder_test(
+            encoder, decoder, freq, DATA_SIZE, encoding_optimality_precision=1e-1, seed=0
+        )
