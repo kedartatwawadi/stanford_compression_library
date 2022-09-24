9a75cda521acad23e097057abd8a244023a448d1
kedar <ktatwawadi@gmail.com>
2022-09-24 11:49:35 -0700

    change name of integer coder to uint coder

--- compressors/universal_uint_coder.py
+++ compressors/universal_uint_coder.py
@@ -18,12 +18,12 @@ The decoding is straightforward, as the unary code indicates how many bits furth
 """
 
 from core.data_block import DataBlock
+from core.data_encoder_decoder import DataDecoder, DataEncoder
 from utils.bitarray_utils import uint_to_bitarray, bitarray_to_uint, BitArray
-from utils.test_utils import try_lossless_compression
-from compressors.prefix_free_compressors import PrefixFreeEncoder, PrefixFreeDecoder
+from utils.test_utils import are_blocks_equal
 
 
-class UniversalUintEncoder(PrefixFreeEncoder):
+class UniversalUintEncoder(DataEncoder):
     """Universal uint encoding:
 
     0 -> 100
@@ -44,8 +44,22 @@ class UniversalUintEncoder(PrefixFreeEncoder):
         len_bitarray = BitArray(len(symbol_bitarray) * "1" + "0")
         return len_bitarray + symbol_bitarray
 
+    def encode_block(self, data_block: DataBlock) -> BitArray:
+        """
+        encode the block of data one symbol at a time
 
-class UniversalUintDecoder(PrefixFreeDecoder):
+        Args:
+            data_block (DataBlock): input block to encoded
+        Returns:
+            BitArray: encoded bitarray
+        """
+
+        encoded_bitarray = BitArray("")
+        for s in data_block.data_list:
+            encoded_bitarray += self.encode_symbol(s)
+        return encoded_bitarray
+
+class UniversalUintDecoder(DataDecoder):
     """Universal uint Decoder
     0 -> 100
     1 -> 101
@@ -77,8 +91,29 @@ class UniversalUintDecoder(PrefixFreeDecoder):
 
         return symbol, num_bits_consumed
 
+    def decode_block(self, bitarray: BitArray):
+        """
+        decode the bitarray one symbol at a time using the decode_symbol
+
+        Args:
+            bitarray (BitArray): input bitarray with encoding of >=1 integers
+
+        Returns:
+            Tuple[DataBlock, Int]: return decoded integers in data block, number of bits read from input
+        """
+        data_list = []
+        num_bits_consumed = 0
+        while num_bits_consumed < len(bitarray):
+            s, num_bits = self.decode_symbol(bitarray[num_bits_consumed:])
+            num_bits_consumed += num_bits
+            data_list.append(s)
+
+        return DataBlock(data_list), num_bits_consumed
 
 def test_universal_uint_encode_decode():
+    """
+    Test if the encoding decoding are lossless
+    """
     encoder = UniversalUintEncoder()
     decoder = UniversalUintDecoder()
 
@@ -86,5 +121,12 @@ def test_universal_uint_encode_decode():
     data_list = [0, 0, 1, 3, 4, 100]
     data_block = DataBlock(data_list)
 
-    is_lossless, _, _ = try_lossless_compression(data_block, encoder, decoder)
-    assert is_lossless
+    # test encode
+    encoded_bitarray = encoder.encode_block(data_block)
+
+    # test decode
+    decoded_block, num_bits_consumed = decoder.decode_block(encoded_bitarray)
+    assert num_bits_consumed == len(encoded_bitarray)
+
+    # compare blocks, and check if the encoding is lossless
+    assert are_blocks_equal(data_block, decoded_block)
