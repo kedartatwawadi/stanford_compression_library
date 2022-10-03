67aea24c2bb4bf7af141c744e867f500c73ea5ec
Shubham Chandak <schandak@stanford.edu>
2022-10-02 18:11:31 -0700

    address PR comments

--- compressors/typical_set_coder.py
+++ compressors/typical_set_coder.py
@@ -4,14 +4,18 @@ For a given iid source $X_1,X_2,...$ i.i.d. $\sim X$ (from alphabet $\mathcal{X}
 and for given values of $n > 0$ and $\epsilon > 0$, the typical set is defined as 
 $$A_{\epsilon}^{(n)} = \left\{x^n | \left|-\frac{1}{n}\log P(x^n) - H(X)\right| \leq \epsilon\right\}$$
 
-This code works by dividing the input into blocks of size n, and then encodes each block
-as follows (note: a single data_block contains several of these n-length blocks):
+This code works by dividing the input into chunks of size n, and then encodes each chunk
+as follows:
 - if $x^n \in A_{\epsilon}^{(n)}$, encode as a $0$ followed by a 
 $\lceil\log_2|A_{\epsilon}^{(n)}|\rceil$ bit representation of the index of $x^n$ in the typical set
 - otherwise, encode as $1$ followed by a $\lceil\log_2|\mathcal{X}|\rceil$ representation 
 of the index of $x^n$ in $\mathcal{X}^n$
+
+Note: a single data_block contains several of these n-length chunks, and in literature often
+these chunks are referred to as blocks (as in block coding).
 """
 
+from attr import dataclass
 from core.data_block import DataBlock
 from core.data_encoder_decoder import DataDecoder, DataEncoder
 from utils.bitarray_utils import BitArray, uint_to_bitarray, bitarray_to_uint
@@ -22,24 +26,48 @@ import itertools
 import random
 
 
-def compute_normalized_negative_log_prob_block(block, prob_dist: ProbabilityDist):
-    """Compute the normalized log probability for a block"""
+def compute_normalized_negative_log_prob_chunk(chunk, prob_dist: ProbabilityDist):
+    """Compute the normalized log probability for a chunk"""
     log_prob = 0
-    for symbol in block:
+    for symbol in chunk:
         log_prob += -np.log2(prob_dist.probability(symbol))
-    return log_prob / len(block)
+    return log_prob / len(chunk)
 
 
-def is_typical(block, prob_dist: ProbabilityDist, eps):
-    """Check if a block is typical given a distribution and value of epsilon"""
-    if np.abs(compute_normalized_negative_log_prob_block(block, prob_dist) - prob_dist.entropy) <= eps:
-        return True
-    else:
-        return False
+def is_typical(chunk, prob_dist: ProbabilityDist, eps):
+    """Check if a chunk is typical given a distribution and value of epsilon"""
+    return (
+        np.abs(compute_normalized_negative_log_prob_chunk(chunk, prob_dist) - prob_dist.entropy)
+        <= eps
+    )
 
 
-def generate_indexes(prob_dist, n, eps):
-    """generate the indexes used for encoding/decoding of typical set code"""
+@dataclass
+class TypicalSetCoderParams:
+    """
+    Parameters for the typical set encoder/decoder.
+    """
+
+    # the chunk size
+    n: int
+
+    # the epsilon used in definition of typical set.
+    # An n-length sequence is typical if its normalized negative
+    # log probability is within eps of the entropy.
+    eps: float
+
+    # the probability distribution used to define typical elements
+    prob_dist: ProbabilityDist
+
+    def __init__(self, n: int, eps: float, prob_dist: ProbabilityDist):
+        """Initialize the typical set coder params"""
+        self.n = n
+        self.eps = eps
+        self.prob_dist = prob_dist
+
+
+def generate_typical_set_coder_lookup_tables(params: TypicalSetCoderParams):
+    """generate the dicts used for encoding/decoding of typical set code"""
     index_typical = {}  # mapping from input typical sequence to its index in the typical set
     index_overall = {}  # mapping from an input sequence to its index in \mathcal{X}^n set
     # both of these map tuples of length n to non-negative integers
@@ -47,9 +75,9 @@ def generate_indexes(prob_dist, n, eps):
     num_typical_seen = 0  # use for indexing typical
     num_overall_seen = 0  # use for indexing overall
 
-    for l in itertools.product(prob_dist.alphabet, repeat=n):
-        if is_typical(l, prob_dist, eps):
-            index_typical[tuple(l)] = num_typical_seen
+    for l in itertools.product(params.prob_dist.alphabet, repeat=params.n):
+        if is_typical(l, params.prob_dist, params.eps):
+            index_typical[tuple(l)] = num_typical_seen  # need to cast to tuple to use as dict key
             num_typical_seen += 1
         index_overall[tuple(l)] = num_overall_seen
         num_overall_seen += 1
@@ -58,13 +86,14 @@ def generate_indexes(prob_dist, n, eps):
 
 
 class TypicalSetEncoder(DataEncoder):
-    """Typical set encoder working on blocks of length n. See module documentation for more details."""
+    """Typical set encoder working on chunks of length n. See module documentation for more details."""
 
-    def __init__(self, prob_dist, n, eps) -> None:
+    def __init__(self, params: TypicalSetCoderParams) -> None:
         super().__init__()
-        self.n = n
-        self.eps = eps
-        self.index_typical, self.index_overall = generate_indexes(prob_dist, n, eps)
+        self.params = params
+        self.index_typical, self.index_overall = generate_typical_set_coder_lookup_tables(
+            self.params
+        )
         # compute lengths of the encoding of the index for typical and non-typical cases
         if len(self.index_typical) == 0:
             self.index_bitlen_typical = None  # empty typical set, so can never encounter
@@ -76,23 +105,25 @@ class TypicalSetEncoder(DataEncoder):
         encoded_bitarray = BitArray("")
         # make sure length is exact multiple of block size n
         assert (
-            data_block.size % self.n == 0
-        ), "Input to typical set encoder should be multiple of block length n"
-        for i in range(0, data_block.size // self.n):
-            block_to_encode = tuple(data_block.data_list[i * self.n : (i + 1) * self.n])
-            if block_to_encode in self.index_typical:
+            data_block.size % self.params.n == 0
+        ), "Input to typical set encoder should be multiple of chunk length n"
+        for chunk_idx in range(0, data_block.size // self.params.n):
+            chunk_to_encode = tuple(
+                data_block.data_list[chunk_idx * self.params.n : (chunk_idx + 1) * self.params.n]
+            )
+            if chunk_to_encode in self.index_typical:
                 # typical case
                 encoded_bitarray += "0"
                 if self.index_bitlen_typical > 0:
                     encoded_bitarray += uint_to_bitarray(
-                        self.index_typical[block_to_encode], self.index_bitlen_typical
+                        self.index_typical[chunk_to_encode], self.index_bitlen_typical
                     )
             else:
                 # non-typical case
                 encoded_bitarray += "1"
                 if self.index_bitlen_overall > 0:
                     encoded_bitarray += uint_to_bitarray(
-                        self.index_overall[block_to_encode], self.index_bitlen_overall
+                        self.index_overall[chunk_to_encode], self.index_bitlen_overall
                     )
         return encoded_bitarray
 
@@ -100,11 +131,12 @@ class TypicalSetEncoder(DataEncoder):
 class TypicalSetDecoder(DataDecoder):
     """Typical set decoder. See module documentation for more details."""
 
-    def __init__(self, prob_dist, n, eps):
+    def __init__(self, params: TypicalSetCoderParams) -> None:
         super().__init__()
-        self.n = n
-        self.eps = eps
-        self.index_typical, self.index_overall = generate_indexes(prob_dist, n, eps)
+        self.params = params
+        self.index_typical, self.index_overall = generate_typical_set_coder_lookup_tables(
+            self.params
+        )
         # compute lengths of the encoding of the index for typical and non-typical cases
         if len(self.index_typical) == 0:
             self.index_bitlen_typical = None  # never going to encounter
@@ -116,16 +148,18 @@ class TypicalSetDecoder(DataDecoder):
         self.inverse_index_typical = {self.index_typical[s]: s for s in self.index_typical}
         self.inverse_index_overall = {self.index_overall[s]: s for s in self.index_overall}
 
-    def decode_block(self, bitarray: BitArray):
+    def decode_block(self, encoded_bitarray: BitArray):
         data_list = []
         num_bits_consumed = 0
-        while num_bits_consumed < len(bitarray):
-            if bitarray[num_bits_consumed] == 0:
+        while num_bits_consumed < len(encoded_bitarray):
+            if encoded_bitarray[num_bits_consumed] == 0:
                 # typical case
                 num_bits_consumed += 1
                 if self.index_bitlen_typical > 0:
                     index_typical = bitarray_to_uint(
-                        bitarray[num_bits_consumed : num_bits_consumed + self.index_bitlen_typical]
+                        encoded_bitarray[
+                            num_bits_consumed : num_bits_consumed + self.index_bitlen_typical
+                        ]
                     )
                 else:
                     index_typical = 0  # if only one sequence
@@ -136,7 +170,9 @@ class TypicalSetDecoder(DataDecoder):
                 num_bits_consumed += 1
                 if self.index_bitlen_overall > 0:
                     index_overall = bitarray_to_uint(
-                        bitarray[num_bits_consumed : num_bits_consumed + self.index_bitlen_overall]
+                        encoded_bitarray[
+                            num_bits_consumed : num_bits_consumed + self.index_bitlen_overall
+                        ]
                     )
                 else:
                     self.index_bitlen_overall = 0  # if only one sequence
@@ -156,31 +192,31 @@ def test_is_typical():
     n = 30
 
     # all A's is not typical
-    block = ["A"] * n
-    assert is_typical(block, prob_dist, eps) == False
+    chunk = ["A"] * n
+    assert is_typical(chunk, prob_dist, eps) == False
 
     # 70% A's, 30% C's is not typical
     num_As = int(0.7 * n)
     num_Cs = n - num_As
-    block = ["A"] * num_As + ["C"] * num_Cs
-    assert is_typical(block, prob_dist, eps) == False
+    chunk = ["A"] * num_As + ["C"] * num_Cs
+    assert is_typical(chunk, prob_dist, eps) == False
     # shuffling doesn't make a difference
-    random.shuffle(block)
-    assert is_typical(block, prob_dist, eps) == False
-    random.shuffle(block)
-    assert is_typical(block, prob_dist, eps) == False
+    random.shuffle(chunk)
+    assert is_typical(chunk, prob_dist, eps) == False
+    random.shuffle(chunk)
+    assert is_typical(chunk, prob_dist, eps) == False
 
     # 60% A's, 30% B's, 10% C's is typical
     num_As = int(0.6 * n)
     num_Bs = int(0.3 * n)
     num_Cs = n - num_As - num_Bs
-    block = ["A"] * num_As + ["B"] * num_Bs + ["C"] * num_Cs
-    assert is_typical(block, prob_dist, eps) == True
+    chunk = ["A"] * num_As + ["B"] * num_Bs + ["C"] * num_Cs
+    assert is_typical(chunk, prob_dist, eps) == True
     # shuffling doesn't make a difference
-    random.shuffle(block)
-    assert is_typical(block, prob_dist, eps) == True
-    random.shuffle(block)
-    assert is_typical(block, prob_dist, eps) == True
+    random.shuffle(chunk)
+    assert is_typical(chunk, prob_dist, eps) == True
+    random.shuffle(chunk)
+    assert is_typical(chunk, prob_dist, eps) == True
 
 
 def test_typical_set_coder_roundtrip():
@@ -199,9 +235,11 @@ def test_typical_set_coder_roundtrip():
                 data_block_size = 1000 * n
                 data_block = get_random_data_block(prob_dist, data_block_size, seed=0)
 
+                params = TypicalSetCoderParams(n, eps, prob_dist)
+
                 # create encoder decoder
-                encoder = TypicalSetEncoder(prob_dist, n, eps)
-                decoder = TypicalSetDecoder(prob_dist, n, eps)
+                encoder = TypicalSetEncoder(params)
+                decoder = TypicalSetDecoder(params)
 
                 # perform compression
                 is_lossless, _, _ = try_lossless_compression(data_block, encoder, decoder)
