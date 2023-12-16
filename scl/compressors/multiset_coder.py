183a3a3eac37028b469b179ebb4a0ad539bb2fa7
Sumer Kohli <sumer.kohli@berkeley.edu>
2023-12-15 23:55:37 -0800

    PR branch documentation

--- scl/compressors/multiset_coder.py
+++ scl/compressors/multiset_coder.py
@@ -78,49 +78,68 @@ def multiset_decode(state: int, multiset_size: int, symbol_decode):
 ######################################## TESTS ##########################################
 
 def generate_e2e_test(multiset_size: int, alphabet_size: int = 5):
+    # Set random seed
     random.seed(42)
+
+    # Generate multiset chars from A to alphabet size with uniform distribution
     per_char = multiset_size // alphabet_size
     assert per_char > 0
     chars = sum([[chr(x)] * per_char for x in range(ord("A"), ord("A") + alphabet_size)], [])
-    # chars = ["a", "a", "b"]
 
+    # Create multiset
     orig_multiset = MultiSetNode.from_iterable(chars)
-    multiset = orig_multiset.clone()
-    # print("Multiset size:", multiset_size)
 
+    # Make a copy because multiset is mutated over course of encoding
+    multiset = orig_multiset.clone()
+    print("Multiset size:", multiset_size)
+
+    # Define symbol encoding as rANS with uniform prior
     def symbol_encode(state, symbol):
-        # Uniform prior
         return rans_encode(state, ord(symbol), 1, 128)
 
+    # Define symbol decoding as rANS with uniform prior
     def symbol_decode(state):
-        # Uniform prior
         symbol = rans_decode_slot(state, 128)
         state = rans_decode(state, symbol, 1, 128)
         return state, symbol
 
+    # Set initial state to this year, why not
     initial_state = 2023
+
+    # Encode multiset
     encoded_state = multiset_encode(initial_state, multiset, symbol_encode)
     # print("Encoded state:", encoded_state)
 
+    # Decode multiset
     output_state, decoded_multiset = multiset_decode(encoded_state, multiset_size, symbol_decode)
+
+    # Check that the final decoded state is the same as the initial state
     assert initial_state == output_state
 
+    # Convert decoded multiset to chars
     decoded_multiset.map_values(chr)
+
+    # Check that the decoded multiset is the same as the original in string representation
     # print("Decoded multiset:", decoded_multiset)
     assert orig_multiset == decoded_multiset
 
+    # Now do naive encoding of input stream in-order with rANS
     char_state = initial_state
     for char in chars:
         char_state = symbol_encode(char_state, char)
     # print("Char state:", char_state)
     encoded_char_state = char_state
 
+    # Do naive decoding of input stream in-order with rANS
     decoded_chars = []
     for _ in range(len(chars)):
         char_state, char = symbol_decode(char_state)
         decoded_chars.append(chr(char))
+
+    # Ensure that the decoded chars are the same as the original (reverse order because decoding is last-in-first-out)
     assert decoded_chars[::-1] == chars
 
+    # Print some fun stats
     print("Multiset bits used:", log2(encoded_state))
     print("Naive bits used:", log2(encoded_char_state))
     print("Improvement (bits):", log2(encoded_char_state) - log2(encoded_state))
